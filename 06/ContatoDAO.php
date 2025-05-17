99de5644afc7a615f8520aa8539099463184af25
aecio <aeciobrito@gmail.com>
2025-05-16 22:03:25 -0300

    Aula 06 - Remocao de contato (by id)

--- 06/ContatoDAO.php
+++ 06/ContatoDAO.php
@@ -58,5 +58,12 @@ class ContatoDAO
         $stmt->bindParam(':endereco', $endereco);
         $stmt->execute();
     }
+
+    public function delete(int $id): void
+    {
+        $stmt = $this->db->prepare("DELETE FROM contatos WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
 }
 ?>
