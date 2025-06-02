e299f2b0f8153bff50af68d5a9fbaccc0c867246
aecio <aeciobrito@gmail.com>
2025-06-02 20:27:30 -0300

    09 - Get By Id

--- 09/api/clientes_api.php
+++ 09/api/clientes_api.php
@@ -5,23 +5,44 @@ require_once __DIR__ . '/../dao/ClienteDAO.php';
 
 $dao = new ClienteDAO();
 $action = $_GET['action'] ?? null;
+$id = isset($_GET['id']) ? $_GET['id'] : null;
 
 switch ($action) {
     case 'listar': // GET
         echo json_encode($dao->getAll());
         break;
+
     case 'buscar': // GET
-        echo json_encode("Chamou o buscar");
+        if($id)
+        {   
+            $cliente = $dao->getById($id);
+            if($cliente)
+                echo json_encode($cliente);
+            else
+            {
+                http_response_code(404);
+                echo json_encode(["error" => "Cliente não encontrado!"]);
+            }
+        }
+        else
+        {
+            http_response_code(400);
+            echo json_encode(["error" => "Você não informou o ID"]);
+        }
         break;
+
     case 'cadastrar': // POST
         echo json_encode("Chamou o cadastrar");
         break;
+
     case 'atualizar': // PUT
         echo json_encode("Chamou o atualizar");
         break;
+
     case 'excluir': // DELETE
         echo json_encode("Chamou o excluir");
         break;
+
     default:
         http_response_code(400);
         echo json_encode(['error' => 'Ação inválida, informar action']);
