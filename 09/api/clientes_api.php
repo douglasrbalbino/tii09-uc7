33fb83ef15be92d743c3327ff63394cdec1fba0f
aecio <aeciobrito@gmail.com>
2025-06-02 19:42:45 -0300

    Aula 09 - Endpoints basicos

--- 09/api/clientes_api.php
+++ 09/api/clientes_api.php
@@ -6,10 +6,21 @@ require_once __DIR__ . '/../dao/ClienteDAO.php';
 $dao = new ClienteDAO();
 $action = $_GET['action'] ?? null;
 
-switch ($action)
-{
-    case 'listar':
+switch ($action) {
+    case 'listar': // GET
         echo json_encode($dao->getAll());
+        break;
+    case 'buscar': // GET
+        echo json_encode("Chamou o buscar");
+        break;
+    case 'cadastrar': // POST
+        echo json_encode("Chamou o cadastrar");
+        break;
+    case 'atualizar': // PUT
+        echo json_encode("Chamou o atualizar");
+        break;
+    case 'excluir': // DELETE
+        echo json_encode("Chamou o excluir");
         break;
     default:
         http_response_code(400);
