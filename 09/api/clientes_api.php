f4401b13c9be8854a1ecd25b410c2de98ab48116
aecio <aeciobrito@gmail.com>
2025-06-02 22:14:45 -0300

    09 - API completa para cliente

--- 09/api/clientes_api.php
+++ 09/api/clientes_api.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../dao/ClienteDAO.php';
 $dao = new ClienteDAO();
 $action = $_GET['action'] ?? null;
 $id = isset($_GET['id']) ? $_GET['id'] : null;
+$inputBody = json_decode(file_get_contents('php://input'), true);
 
 switch ($action) {
     case 'listar': // GET
@@ -13,34 +14,70 @@ switch ($action) {
         break;
 
     case 'buscar': // GET
-        if($id)
-        {   
+        if ($id) {
             $cliente = $dao->getById($id);
-            if($cliente)
+            if ($cliente)
                 echo json_encode($cliente);
-            else
-            {
+            else {
                 http_response_code(404);
                 echo json_encode(["error" => "Cliente não encontrado!"]);
             }
-        }
-        else
-        {
+        } else {
             http_response_code(400);
             echo json_encode(["error" => "Você não informou o ID"]);
         }
         break;
 
     case 'cadastrar': // POST
-        echo json_encode("Chamou o cadastrar");
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && $inputBody) {
+            $cli = new Cliente(null, $inputBody['nome'], $inputBody['cpf'], $inputBody['dataDeNascimento'], $inputBody['ativo']);
+            if($dao->create($cli))
+            {
+                http_response_code(201);
+                echo json_encode(['success' => 'Cliente cadastrado']);
+            }
+            else 
+            {
+                http_response_code(500);
+                echo json_encode(['error' => 'Cliente não cadastrado!']);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(['error' => 'Método incorreto']);
+        }
         break;
 
     case 'atualizar': // PUT
-        echo json_encode("Chamou o atualizar");
+        if ($_SERVER['REQUEST_METHOD'] == 'PUT' && $inputBody && $id) {
+            $cli = new Cliente($id, $inputBody['nome'], $inputBody['cpf'], $inputBody['dataDeNascimento'], $inputBody['ativo']);
+            if($dao->update($cli))
+            {
+                http_response_code(204);
+                echo json_encode(['success' => 'Cliente atualizado']);
+            }
+            else 
+            {
+                http_response_code(500);
+                echo json_encode(['error' => 'Cliente não atualizado!']);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(['error' => 'ID não fornecido, ou método incorreto']);
+        }
         break;
 
     case 'excluir': // DELETE
-        echo json_encode("Chamou o excluir");
+        if ($id && $_SERVER['REQUEST_METHOD'] == 'DELETE') {
+            if ($dao->delete($id)) {
+                echo json_encode(['message' => 'Cliente excluído!']);
+            } else {
+                http_response_code(500);
+                echo json_encode(['error' => 'Erro ao excluir!']);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(['error' => 'ID não fornecido ou método incorreto']);
+        }
         break;
 
     default:
