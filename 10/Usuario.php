697b1f66906bd1a319d74c4e026fdcaa025eae1d
aecio <aeciobrito@gmail.com>
2025-06-04 22:18:29 -0300

    Criacao de tabela e modelo de usuario

---
+++ 10/Usuario.php
@@ -0,0 +1,20 @@
+<?php
+
+class Usuario
+{
+    private ?int $id;
+    private string $nome;
+    private string $senha;
+    private string $email;
+    private ?string $token;
+
+
+    public function __construct(?int $id, string $nome, string $senha, string $email, ?string $token = null) 
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->senha = $senha;
+        $this->email = $email;
+        $this->token = $token;
+    }
+}
