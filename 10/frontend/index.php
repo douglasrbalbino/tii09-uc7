c39f5efceb339d5a3a547b66c0154ca9d1638cb9
aecio <aeciobrito@gmail.com>
2025-06-11 21:43:50 -0300

    Login de sessao de usuario

--- 10/frontend/index.php
+++ 10/frontend/index.php
@@ -1,12 +1,19 @@
 <?php
+session_start();
 
+$isLogged = isset($_SESSION['token']);
 ?>
 
 <h1>Home</h1>
 
 <nav>
     <a href="index.php">Home</a>
-    <a href="index.php">Login</a>
-    <a href="cadastro.php">Cadastrar</a>
+    <?php if ($isLogged): ?>
+        <a href="index.php">Minha Conta</a>
+        <a href="cadastro.php">Sair</a>
+    <?php else: ?>
+        <a href="login.php">Login</a>
+        <a href="cadastro.php">Cadastrar</a>
+    <?php endif; ?>
 </nav>
 <p>Bem-vindo ao sistema!</p>
